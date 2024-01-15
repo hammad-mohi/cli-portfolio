33c997ef47205fb2ef4583b2d47648233b6c4699
Hammad Mohiuddin <hammad.mohiuddin@gmail.com>
2024-01-14 17:04:32 -0800

    Adding tailwindcss and twin.macro

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,22 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020',
+    },
+  },
+  esbuild: {
+    // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
   plugins: [
-    react(),
+    react(
+      {
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
     VitePWA({
       registerType: "autoUpdate",
     }),
